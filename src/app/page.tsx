9d39c1e86fcdc71f131574033766714062932b55
64576 <64576@etu.he2b.be>
2025-08-16 21:44:30 +0200

    zebi enfin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ import Image from "next/image";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCo0Slu-sw1Bj3yrBEpnunUSkCv1Mbzoco",
-  authDomain: "books-and-bites-review.firebaseapp.com",
-  projectId: "books-and-bites-review",
-  storageBucket: "books-and-bites-review.appspot.com",
-  messagingSenderId: "714302422646",
-  appId: "1:714302422646:web:046f51a7003578eb942957",
-  measurementId: "G-XGLFCB1X61",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
